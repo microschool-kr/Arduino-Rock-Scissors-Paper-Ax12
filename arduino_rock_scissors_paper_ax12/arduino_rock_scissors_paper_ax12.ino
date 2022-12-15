216f31a448a187c43a4dd7c550a01d2a3ca7f5e6
juchajam <79612680+juchajam@users.noreply.github.com>
2022-12-15 11:36:47 +0900

    Update arduino_rock_scissors_paper_ax12.ino

--- arduino_rock_scissors_paper_ax12/arduino_rock_scissors_paper_ax12.ino
+++ arduino_rock_scissors_paper_ax12/arduino_rock_scissors_paper_ax12.ino
@@ -5,21 +5,22 @@
 #define ROCK 2
 #define PAPER 14
 
+#define NUM_MOTORS 3
+
 #define UP_SPEED 200
-#define DOWN_SPEED 100
-#define UP_POSITION 204
-#define DOWN_POSITION 490
+#define DOWN_SPEED 130
+#define UP_POSITION 195
+#define DOWN_POSITION 500
 
 softHalfDuplexSerial port(8); // data pin 8
 dxlAx dxlCom(&port);
 
-
-int count = 0;
-int old_count = 0;
+bool state = false;
 int distance = 0;
-int min_distance = 0;
 int max_distance = 12;
 long randNumber;
+int motors[NUM_MOTORS] = {SCISSOR, ROCK, PAPER};
+int counts[NUM_MOTORS] = {0, 0, 0};
 
 void setup() {
   Serial.begin(57600);
@@ -34,46 +35,37 @@ void setup() {
 }
 
 void loop() {
-  ///////////////초음파 센서 인식/////////////
   int volt = map(analogRead(A0), 0, 1023, 0, 5000);
   distance = (27.61 / (volt - 0.1696)) * 1000;
   //Serial.println(distance);
 
-  if (distance >= min_distance && distance <= max_distance)
+  if (distance <= max_distance && !state)
   {
-    count = 1;
-  }
-  else
-  {
-    count = 0;
-  }
-
-  if (count != old_count)
-  {
-    if (count && !old_count)
+    randNumber = random(0, 3);
+    // Serial.println(randNumber);
+    if (++counts[randNumber] == 4)
     {
-      randNumber = random(1, 4);
-      // Serial.println(randNumber);
-      if (randNumber == 1)  // 가위
+      int a = (randNumber + 1) % 3;
+      int b = (randNumber + 2) % 3;
+      if (counts[a] == counts[b])
       {
-        upMotor(SCISSOR);
+        int arr[2] = {a, b};
+        randNumber = arr[random(0, 2)];
       }
-      else if (randNumber == 2) // 바위
+      else
       {
-        upMotor(ROCK);
+        randNumber = counts[a] < counts[b] ? a : b;
       }
-      else if (randNumber == 3) // 보
-      {
-        upMotor(PAPER);
-      }
-      delay(1000);
+      counts[0] = counts[1] = counts[2] = 0;
     }
-    else if (!count && old_count) 
-    {
-      downAllMotors();
-    }
-
-    old_count = count;
+    upMotor(motors[randNumber]);
+    state = true;
+    delay(1000);
+  }
+  else if (distance > max_distance && state)
+  {
+    downAllMotors();
+    state = false;
   }
 }
 
@@ -97,37 +89,26 @@ void downMotor(byte id)
 
 void downAllMotors()
 {
-  dxlCom.setGoalPositionAtSpeed(1, DOWN_POSITION, DOWN_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
-  dxlCom.setGoalPositionAtSpeed(2, DOWN_POSITION, DOWN_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
-  dxlCom.setGoalPositionAtSpeed(14, DOWN_POSITION, DOWN_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
+  for (int i = 0; i < NUM_MOTORS; i++)
+  {
+    dxlCom.setGoalPositionAtSpeed(motors[i], DOWN_POSITION, DOWN_SPEED);
+    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
+    dxlCom.readDxlError();
+    dxlCom.readDxlResult();
+  }
   bool isMoving = true;
   while (isMoving)
   {
     unsigned short error = DXL_ERR_SUCCESS;
+    isMoving = false;
     while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(SCISSOR);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving = dxlCom.readDxlResult();
-    while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(ROCK);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving |= dxlCom.readDxlResult();
-    while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(PAPER);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving |= dxlCom.readDxlResult();
+    for (int i = 0; i < NUM_MOTORS; i++)
+    {
+      dxlCom.isMoving(motors[i]);
+      while(!dxlCom.dxlDataReady());        // waiting the answer of servo
+      error = dxlCom.readDxlError();
+      isMoving |= dxlCom.readDxlResult();
+    }
   }
 }
 
@@ -151,36 +132,25 @@ void upMotor(byte id)
 
 void upAllMotors()
 {
-  dxlCom.setGoalPositionAtSpeed(1, UP_POSITION, UP_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
-  dxlCom.setGoalPositionAtSpeed(2, UP_POSITION, UP_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
-  dxlCom.setGoalPositionAtSpeed(14, UP_POSITION, UP_SPEED);
-  while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-  dxlCom.readDxlError();
-  dxlCom.readDxlResult();
+  for (int i = 0; i < NUM_MOTORS; i++)
+  {
+    dxlCom.setGoalPositionAtSpeed(motors[i], UP_POSITION, UP_SPEED);
+    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
+    dxlCom.readDxlError();
+    dxlCom.readDxlResult();
+  }
   bool isMoving = true;
   while (isMoving)
   {
     unsigned short error = DXL_ERR_SUCCESS;
+    isMoving = false;
     while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(SCISSOR);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving = dxlCom.readDxlResult();
-    while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(ROCK);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving |= dxlCom.readDxlResult();
-    while(dxlCom.isBusy()); // waiting the status return delay time (for testing if it is moving)
-    dxlCom.isMoving(PAPER);
-    while(!dxlCom.dxlDataReady());        // waiting the answer of servo
-    error = dxlCom.readDxlError();
-    isMoving |= dxlCom.readDxlResult();
+    for (int i = 0; i < NUM_MOTORS; i++)
+    {
+      dxlCom.isMoving(motors[i]);
+      while(!dxlCom.dxlDataReady());        // waiting the answer of servo
+      error = dxlCom.readDxlError();
+      isMoving |= dxlCom.readDxlResult();
+    }
   }
 }
